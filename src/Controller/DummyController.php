639e1136c0bcf76aeb7c854cca097953a133311b
ZEHOUANI RIHAB <rihabze79@gmail.com>
2026-01-13 17:57:20 +0100

    firt commit

--- src/Controller/DummyController.php
+++ src/Controller/DummyController.php
@@ -1,18 +1,21 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DummyController extends AbstractController
-{
-    #[Route('/dummy', name: 'app_dummy')]
-    public function index(): Response
-    {
-        return $this->render('dummy/index.html.twig', [
-            'controller_name' => 'DummyController',
-        ]);
-    }
+class DummyController extends AbstractController{
+   #[Route(path: '/add-book',name: 'app_book_add')] 
+   public function addBook(): Response{
+      $book = new Book();
+      $bookForm = $this->createForm(type: BookType::class, data: $book);
+    return $this->render(view: 'orange/index.html.twig',parameters:[
+      "bookForm" => $bookForm
+    ]) ;
+   }
 }
+
+
+
+?>
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,18 +1,17 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class IndexController extends AbstractController
-{
-    #[Route('/index', name: 'app_index')]
-    public function index(): Response
-    {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
-    }
+class IndexController extends AbstractController{
+   #[Route(path: '/index/{name}',name: 'app_index',requirements: ['name'=>'\d+'])] 
+   public function index(int $name= 1): Response{
+    return new Response(content : "Hello" .$name . "!");
+   }
 }
+
+
+
+?>
